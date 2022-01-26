b2153a90d7a62694a561921a6bcfa70b81665384
Eugene A. Kuchin <e.kuchin@gmail.com>
2022-01-26 10:08:17 +0500

    Generate Deck

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,80 @@
 import { createStore } from "vuex";
+import Card from "@/model/Card";
+import { Rank, Color, EnumToArray } from "@/model/Card";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  
+  state: {
+    currentCard: {} as Card,
+    deck: [] as Card[], //Array of Cards
+    isClockwise: true,
+    players: [] as Array<Array<Card>>,
+  },
+  
+  getters: {
+    getCurrentCard(state):Card {
+      return state.currentCard;
+    },
+    getDeck(state):Card[] {
+      return state.deck;
+    }
+  },
+   
+  mutations: {
+    addCardToDeck(state, payload){
+      state.deck.push(payload)
+    }
+    //removeCardToDeck()
+    //setCurrentCard(card)
+    //addCardToPlayer(card)
+    //removeCardFromPlayer(index)
+  },
+  
+  actions: {
+    //Сгенерировать колоду
+    generateNewDeck (context) {
+      
+      //zero
+      for (let color = 1; color <= 4; color++){
+        context.commit('addCardToDeck', {rank:0,color})
+      }
+
+      //numeric
+      for (let i = 0; i <= 1; i++){
+        for (let rank = 1; rank <= 9; rank++) {
+          for (let color = 1; color <= 4; color++){
+            context.commit('addCardToDeck', {rank,color})
+          }
+        }
+      }
+
+      //special
+      let arr = [Rank.Skip, Rank.Plus2, Rank.Reverse]
+      for (let i = 0; i <= 1; i++){
+        for (const rank in arr) {
+          for (let color = 1; color <= 4; color++){
+            context.commit('addCardToDeck', {rank:arr[rank], color})
+          }
+        }
+      }
+
+      //black
+      arr = [Rank.Plus4, Rank.SelectColor]
+      for (let i = 0; i <= 3; i++){
+        for (const rank in arr) {
+          context.commit('addCardToDeck', {rank:arr[rank], color:Color.Black})
+        }
+      }
+      //console.log(EnumToArray(Color));    
+    },
+    
+    //Перетасовать колоду
+    shuffleDeck (context) {
+            
+    }
+    
+    //Переместить N карт игроку M из колоды
+
+  },
   modules: {},
 });
